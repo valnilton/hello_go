e292dc2facbb11f02c9cfbd62d2f5ebb4b4f71dc
Valnilton Evilásio <valnilton@gmail.com>
2017-07-28 10:27:00 -0300

    Um tour por Go: Básico 11/17)

--- hello.go
+++ hello.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math/cmplx"
+)
+
+var (
+    ToBe    bool        = false
+    MaxInt  uint64      = 1<<64 - 1 
+    c, java int         = 2, 3
+    z       complex128  = cmplx.Sqrt(5-2i)
+)
+
+func split(sum int) (x,y int) {
+    x = sum *4 / 9
+    y = sum - x
+    return x, y
+}
 
 func main() {
-	fmt.Printf("Hello, val.\n")
+    var i = "9n"
+    k := "tres"
+	fmt.Println(split(14))
+	fmt.Println(c, java, i, k)
+	fmt.Printf("\nTipo %T, valor = %v\n", ToBe, ToBe)
+	fmt.Printf("Tipo %T, valor = %v\n", MaxInt, MaxInt)
+	fmt.Printf("Tipo %T, valor = %v\n", z, z)
 }
